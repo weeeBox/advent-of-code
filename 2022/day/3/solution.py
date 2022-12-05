3e4f12edb119bfa30dc18b4f5d2fc2776658ab96
Alex Lementuev <a.lementuev@gmail.com>
2022-12-04 19:16:22 -0800

    Day 3 - part 2

--- 2022/day/3/solution.py
+++ 2022/day/3/solution.py
@@ -4,7 +4,7 @@ from typing import Tuple
 sys.path.append('../../helpers')
 
 from helpers import read_values
-from collections import Counter, namedtuple
+from collections import defaultdict, namedtuple
 
 Rucksack = namedtuple('Rucksack', ('first', 'second'))
 
@@ -29,10 +29,35 @@ def _get_rucksack_sum(rucksack):
     return 0
 
 
-def _get_sum_of_priorities(rucksacks):
+def _get_sum_of_priorities(rucksacks) -> int:
     total = 0
     for rucksack in rucksacks:
         total += _get_rucksack_sum(rucksack)
+    return total
+
+
+def _get_group_token(group):
+    counts = defaultdict(int)
+
+    def get_unique(rucksack: Rucksack) -> set:
+        return set(rucksack.first + rucksack.second)
+
+    for unique in map(get_unique, group):
+        for item in unique:
+            counts[item] += 1
+
+    for item, count in counts.items():
+        if count == len(group):
+            return item
+
+    raise ValueError('Token not found')
+
+
+def _get_sum_of_token_priorities(groups) -> int:
+    total = 0
+    for group in groups:
+        token = _get_group_token(group)
+        total += _get_priority(token)
     return total
 
 
@@ -44,3 +69,17 @@ def _parse_rucksack(value: str) -> Rucksack:
 def get_sum_of_priorities(path: str) -> int:
     rucksacks = read_values(path, _parse_rucksack)
     return _get_sum_of_priorities(rucksacks)
+
+
+def _group_rucksacks(rucksacks, group_size) -> tuple:
+    res = []
+    for group_index in range(len(rucksacks) // group_size):
+        res.append(rucksacks[group_index * group_size: (group_index + 1) * group_size])
+
+    return tuple(res)
+
+
+def get_sum_of_token_priorities(path: str) -> int:
+    rucksacks = read_values(path, _parse_rucksack)
+    groups = _group_rucksacks(rucksacks, group_size=3)
+    return _get_sum_of_token_priorities(groups)
--- 2022/day/3/solution_test.py
+++ 2022/day/3/solution_test.py
@@ -6,7 +6,7 @@ from typing import Tuple
 sys.path.append('../../helpers')
 
 from helpers import read_int
-from solution import get_sum_of_priorities
+from solution import get_sum_of_priorities, get_sum_of_token_priorities
 
 
 class MyTestCase(unittest.TestCase):
@@ -22,12 +22,12 @@ class MyTestCase(unittest.TestCase):
 
     def test_score_small_input_part_2(self):
         expected = read_int('test/output-small-part2.txt')
-        actual = solve_part2('test/input-small.txt')
+        actual = get_sum_of_token_priorities('test/input-small.txt')
         self.assertEqual(expected, actual)
 
     def test_score_large_input_part_2(self):
         expected = read_int('test/output-large-part2.txt')
-        actual = solve_part2('test/input-large.txt')
+        actual = get_sum_of_token_priorities('test/input-large.txt')
         self.assertEqual(expected, actual)
 
 
